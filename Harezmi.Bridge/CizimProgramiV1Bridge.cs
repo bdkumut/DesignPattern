2db0c9d6cd3417cd2242e90591892ae5f991343c
Umut Budak <umut.budak@sahibinden.com>
2015-11-07 11:50:57 +0300

    Bridge Pattern'i güncellendi.

--- Harezmi.Bridge/CizimProgramiV1Bridge.cs
+++ Harezmi.Bridge/CizimProgramiV1Bridge.cs
@@ -7,7 +7,8 @@ namespace Harezmi.Bridge
 {
     public class CizimProgramiV1Bridge:ICizimProgrami
     {
-        private CizimProgramiV1 target = new CizimProgramiV1();
+        //private CizimProgramiV1 target = new CizimProgramiV1();
+        private CizimProgramiV2 target = new CizimProgramiV2();
 
         public void CizgiCiz()
         {
---
+++ Harezmi.Bridge/CizimProgramiV2.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Harezmi.Bridge
+{
+    public class CizimProgramiV2
+    {
+        public void CizgiCek()
+        {
+            Console.WriteLine("CizimProgrami V2 ile çizgi çiziliyor");
+        }
+
+        public void CemberCizdir()
+        {
+            Console.WriteLine("CizimProgrami V2 ile çember çiziliyor");
+        }
+    }
+}
