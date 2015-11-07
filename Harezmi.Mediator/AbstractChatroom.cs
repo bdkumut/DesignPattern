749a8b0ce4ec02be9fb8bce1e0f1f9da597047e0
Umut Budak <umut.budak@sahibinden.com>
2015-11-07 11:11:02 +0300

    Mediator Pattern'i eklendi.

---
+++ Harezmi.Mediator/AbstractChatroom.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Harezmi.Mediator
+{
+    abstract class AbstractChatroom
+    {
+        public abstract void Register(Participant participant);
+        public abstract void Send(string from, string to, string message);
+    }
+}
